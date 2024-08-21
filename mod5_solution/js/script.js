518113eece68ace8f609bc18888b5d500d9b3cad
Osama-Aljandali <osamaaljendaly@gmail.com>
2024-08-21 16:18:50 +0300

    Updated js/script.js file

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -80,7 +80,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 // *** start ***
 // On first load, show home view
-document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
